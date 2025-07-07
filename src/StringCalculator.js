988a31ada28545cedf7d99fee53d9d50443e9ed3
Rohit Panikar <rohit.paniker.1990@gmail.com>
2025-07-07 16:39:02 +0530

    Test passes after refactoring user input to make it more readable

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,25 +1,25 @@
-// The code is already clean and simple. 
-// Refactoring will become more 
-// important as we add more test cases and functionality.
 
 class StringCalculator {
   add(numbers) {
     // Test for empty input. 
     // Minimal implementation to make the test pass
     
-    if (numbers === "") {
+    // REFACTORING user input for better readability.
+    // This will extract clean numbers mixed in commas, newline or any special char delimiters
+    const _numbers = (numbers.length === 0) ? numbers : numbers.match(/\d+/g).join()
+    
+    if (_numbers === "") {
       return 0;
     }
 
     // Handle single, comma, newline or any delimiter separated numbers using Regex
-    if (numbers.match(/\d+/g).join().includes(",")) {
-        const onlyNumbers = numbers.match(/\d+/g).join();
-        const parts = onlyNumbers.split(",");
+    if (_numbers.includes(",")) {
+        const parts = _numbers.split(",");
         const addAll = parts.reduce((accumlator, currentValue) => accumlator = parseInt(accumlator) + parseInt(currentValue));
         return parseInt(addAll);
     }
     
-    return parseInt(numbers);
+    return parseInt(_numbers);
     // We'll add more logic later for other cases
   }
 }
