128b022fe72a88049d59dc4b9c4685f5c8c08a94
Rohit Panikar <rohit.paniker.1990@gmail.com>
2025-07-07 14:42:49 +0530

    Test fail - no StringCalculator.js file created

---
+++ test/StringCalculator.test.js
@@ -0,0 +1,16 @@
+// RED PHASE: Write the failing test first
+
+const StringCalculator = require('../src/StringCalculator');
+
+describe('StringCalculator', () => {
+  let calculator;
+
+  beforeEach(() => {
+    calculator = new StringCalculator();
+  });
+
+  test('should return 0 for empty string', () => {
+    // This test will FAIL initially (RED phase) since no StringCalculator.js file is created
+    expect(calculator.add("")).toBe(0);
+  });
+});
