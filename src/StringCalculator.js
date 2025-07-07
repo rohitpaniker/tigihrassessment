6cc1f4e0208ca83b424b91581a60188cd8b7b34c
Rohit Panikar <rohit.paniker.1990@gmail.com>
2025-07-07 17:01:55 +0530

    Test fails for not throwing error since Regex fails to extract -ve numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,27 +1,31 @@
 
 class StringCalculator {
-  add(numbers) {
-    // Test for empty input. 
-    // Minimal implementation to make the test pass
-    
-    // REFACTORING user input for better readability.
-    // This will extract clean numbers mixed in commas, newline or any special char delimiters
-    const _numbers = (numbers.length === 0) ? numbers : numbers.match(/\d+/g).join()
-    
-    if (_numbers === "") {
-      return 0;
-    }
+    add(numbers) {
+        // This will extract clean numbers mixed in commas, newline or any special char delimiters
+        const _numbers = (numbers.length === 0) ? numbers : numbers.match(/\d+/g).join()
 
-    // Handle single, comma, newline or any delimiter separated numbers using Regex
-    if (_numbers.includes(",")) {
-        const parts = _numbers.split(",");
-        const addAll = parts.reduce((accumlator, currentValue) => accumlator = parseInt(accumlator) + parseInt(currentValue));
-        return parseInt(addAll);
+        if (_numbers === "") {
+            return 0;
+        }
+        
+        const numArray = _numbers.split(",");
+        
+        // Negative number check
+        const negatives = numArray.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error(`negative numbers not allowed ${negatives.join(',')}`)
+        }
+
+        // Process if no negative numbers
+        // Handle single, comma, newline or any delimiter separated numbers using Regex
+        if (_numbers.includes(",")) {
+            const addAll = numArray.reduce((accumlator, currentValue) => accumlator = parseInt(accumlator) + parseInt(currentValue));
+            return parseInt(addAll);
+        }
+
+        return parseInt(_numbers);
+        // We'll add more logic later for other cases
     }
-    
-    return parseInt(_numbers);
-    // We'll add more logic later for other cases
-  }
 }
 
 module.exports = StringCalculator;
