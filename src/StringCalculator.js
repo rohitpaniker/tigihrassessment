6842f83d4859fe9d3f597acafe122e279d3eac70
Rohit Panikar <rohit.paniker.1990@gmail.com>
2025-07-07 14:50:38 +0530

    Test passed for 1st case: checking empty input to StringCalculator

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,13 @@
 class StringCalculator {
   add(numbers) {
+    // Test for empty input. 
+    // Minimal implementation to make the test pass
     
+    if (numbers === "") {
+      return 0;
+    }
+
+    // We'll add more logic later for other cases
   }
 }
 
